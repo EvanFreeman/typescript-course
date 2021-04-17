e84d489028efaf301a63976cd1eac5f92a8977f4
Evan Freeman <evan.e.freeman@gmail.com>
2021-04-17 10:43:11 -0400

    Add markers

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,6 +1,17 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
+// Instructions to every other class
+// on how they can be an argument to 'addMarker'
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
-
 
   constructor(divId: string) {
     const element = document.getElementById(divId);
@@ -10,4 +21,23 @@ export class CustomMap {
     });
   }
 
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+  }
+
+  /* addCompanyMarker(company: Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: company.location.lat,
+        lng: company.location.lng,
+      },
+    });
+  } */
 }
