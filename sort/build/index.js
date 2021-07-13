fc53f61f09188b8c19fee152a8e2366efd3aca19
Evan Freeman <evan.e.freeman@gmail.com>
2021-07-12 21:59:35 -0400

    Refactor sorter implementation

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,1 +1,8 @@
-console.log('hi there');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var NumberCollection_1 = require("./NumberCollection");
+var Sorter_1 = require("./Sorter");
+var col = new NumberCollection_1.NumberCollection([10, 3, -5, 0]);
+var sorter = new Sorter_1.Sorter(col);
+sorter.sort();
+console.log(col.data);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,1 +1,7 @@
-console.log('hi there');
+import { NumberCollection } from "./NumberCollection";
+import { Sorter } from "./Sorter";
+
+const col = new NumberCollection([10, 3, -5, 0]);
+const sorter = new Sorter(col);
+sorter.sort();
+console.log(col.data);
